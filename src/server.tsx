1ae2dfb6b494e5bba6aab60e091ccb200f18c818
NicoleTeisen <nicole.camargo@gmail.com>
2024-07-15 20:01:49 -0300

    Test database with create and list routes

--- src/server.tsx
+++ src/server.tsx
@@ -1,9 +1,24 @@
 import fastify from "fastify";
+import { prisma } from "./lib/prisma";
 
 const app = fastify();
 
-app.get("/teste", () => {
-  return "Hello word";
+app.get("/cadastrar", async () => {
+  await prisma.trip.create({
+    data: {
+      destination: "São Paulo",
+      starts_at: new Date(),
+      ends_at: new Date(),
+    },
+  });
+
+  return "Registro criado com sucesso!";
+});
+
+app.get("/listar", async () => {
+  const trips = await prisma.trip.findMany();
+
+  return trips;
 });
 
 app.listen({ port: 3333 }).then(() => {
